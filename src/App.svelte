3499a20bc14e7f2d4d1b3c98f929c8029c54fb3b
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-04-15 11:41:32 +0200

    switched to use single fetch for data instead of multiple

--- src/App.svelte
+++ src/App.svelte
@@ -2,28 +2,20 @@
   import { onMount } from 'svelte';
   import MetricCard from './components/MetricCard.svelte';
 
-  let outboundCalls = null;
-  let leadsContacted = null;
+  let weeklyCalls = null;
+  let weeklyContactedLeads = null;
 
   onMount(async () => {
-  const resCalls = await fetch('http://localhost:8080/api/calls-this-week');
-  const jsonCalls = await resCalls.json();
-  outboundCalls = jsonCalls.data;
-
-  const resLeads = await fetch('http://localhost:8080/api/contacted-leads-this-week');
-  const jsonLeads = await resLeads.json();
-  leadsContacted = jsonLeads.data;
+  const res = await fetch('http://localhost:8080/api/dashboard-data');
+  const json = await res.json();
+  weeklyCalls = json.data.weeklyCalls;
+  weeklyContactedLeads = json.data.weeklyContactedLeads;
 });
 </script>
 
-{#if outboundCalls !== null}
-  <MetricCard title="Outbound Calls This Week" value={outboundCalls} />
-{:else}
-  <p>Loading...</p>
-{/if}
-
-{#if leadsContacted !== null}
-  <MetricCard title="Leads contacted This Week" value={leadsContacted} />
+{#if weeklyCalls !== null && weeklyContactedLeads !== null}
+  <MetricCard title="Outbound Calls This Week" value={weeklyCalls} />
+  <MetricCard title="Leads Contacted This Week" value={weeklyContactedLeads} />
 {:else}
   <p>Loading...</p>
 {/if}
