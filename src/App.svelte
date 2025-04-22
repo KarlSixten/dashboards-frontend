457d698e0d661dbd06352ce4e6737bb12f5157a5
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-04-22 15:19:51 +0200

    Added routing for viewing different groups data

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,20 @@
 <script>
+    import { Router, Route } from 'svelte-routing'
     import Dashboard from "./pages/Dashboard/Dashboard.svelte";
 
+    export let url = "";
 </script>
 
-<Dashboard group="sales"></Dashboard>
+<Router {url}>
+    <div>
+        <Route path="/">
+            <Dashboard group="sales"></Dashboard>
+        </Route>
+        <Route path="/sales">
+            <Dashboard group="sales"></Dashboard>
+        </Route>
+        <Route path="/outreach">
+            <Dashboard group="outreach"></Dashboard>
+        </Route>
+    </div>
+</Router>
